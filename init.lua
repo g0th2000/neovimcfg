0d0b548678dae75d76aea5152a63b5baebbdfb4f
g0th2000 <git@gothmog.org.uk>
2025-08-11 12:16:45 +0100

    Enable copilot, use LazyExtras for Overseer and fix UndoTree label

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("lsp.clangd")
+require("lsp.basedpyright")
 require("config.undotree")
 require("config.vim-fugitive")
 
