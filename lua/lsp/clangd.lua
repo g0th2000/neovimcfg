b07b18b4618b4f23a097a8fee1e1e3da599df8bb
g0th2000 <git@gothmog.org.uk>
2026-01-05 00:11:43 +0000

    Fixes
    mini-icons renamed
    remove legacy copilot
    tidied up formatting

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -1,16 +1,20 @@
 local buildDir = os.getenv("BUILDDIR")
+local oeCore = os.getenv("OECORE_NATIVE_SYSROOT")
 local queryDriver = nil
 if buildDir ~= nil then
-    queryDriver = "--query-driver=" .. buildDir .. "/**/arm-rdk-linux-gnueabi-g++"
+	queryDriver = "--query-driver=" .. buildDir .. "/**/arm-rdk-linux-gnueabi-g++"
+end
+
+if oeCore ~= nil then
+	-- queryDriver = "--query-driver=" .. oeCore .. "/usr/bin/aarch64-rdk-linux/aarch64-rdk-linux-g++"
+	queryDriver = "--query-driver=" .. oeCore .. "/usr/bin/arm-rdk-linux-gnueabi/arm-rdk-linux-gnueabi-g++"
 end
 
 local androidDir = os.getenv("ANDROID_CLANG_BIN")
 if androidDir ~= nil then
-    queryDriver = "--query-driver=" .. androidDir
+	queryDriver = "--query-driver=" .. androidDir
 end
 
 require("lspconfig").clangd.setup({
-    cmd = { "clangd",
-        queryDriver,
-    },
+	cmd = { "clangd", queryDriver },
 })
